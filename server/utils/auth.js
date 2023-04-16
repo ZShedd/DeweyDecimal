bfa7a3b34bd74adfadbc7051ac0d4daffe6eb0d7
Clown Supreme <zachary.h.shedd@gmail.com>
2023-04-15 19:57:43 -0700

    Rewrote token functions in auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,32 +1,30 @@
 const jwt = require("jsonwebtoken");
 
-// set token secret and expiration date
 const secret = "mysecretsshhhhh";
 const expiration = "2h";
 
 module.exports = {
-  authMiddleware: function ({ context }, next) {
-    const token = context.token;
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
 
     if (!token) {
-      return {
-        success: false,
-        message: "You have no token",
-      };
+      return req;
     }
 
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      context.user = data;
+      req.user = data;
     } catch {
       console.log("Invalid token");
-      return {
-        success: false,
-        message: "Invalid token",
-      };
     }
 
-    return next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
